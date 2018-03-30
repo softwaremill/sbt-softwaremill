99863ba238eb562dc1bd67d1d7122b3a9594f088
Krzysiek Ciesielski <kciesielski@users.noreply.github.com>
2018-03-30 13:09:34 +0200

    Support -Ypartial-unification

--- src/main/scala/com/softwaremill/SbtSoftwareMill.scala
+++ src/main/scala/com/softwaremill/SbtSoftwareMill.scala
@@ -66,6 +66,7 @@ object SbtSoftwareMill extends AutoPlugin {
       "-Ywarn-unused:imports",         // Warn if an import selector is not referenced.
       "-Ywarn-unused:locals",          // Warn if a local definition is unused.
       "-Ywarn-unused:params",          // Warn if a value parameter is unused.
+      "-Ypartial-unification",         // Improves type constructor inference with support for partial unification (SI-2712)
       "-Ywarn-unused:patvars",         // Warn if a variable bound in a pattern is unused.
       "-Ywarn-unused:privates",        // Warn if a private member is unused.
       "-Ywarn-extra-implicit")         // Warn when more than one implicit parameter section is defined.
