107348d75c63b03971c0024f704bb7015c5cc826
ghostbuster91 <kghost0@gmail.com>
2020-05-06 09:10:47 +0200

    Expose beforeCommit hook

--- publish/src/main/scala/com/softwaremill/PublishTravis.scala
+++ publish/src/main/scala/com/softwaremill/PublishTravis.scala
@@ -23,45 +23,56 @@ trait PublishTravis {
       "A hacky way to differentiate between commitRelease and publishRelease invocations."
     )
 
-  lazy val publishTravisSettings = Seq(
-    isCommitRelease := true,
-    useGpg := false, // use the gpg implementation from the sbt-pgp plugin
-    pgpSecretRing := baseDirectory.value / "secring.asc", // unpacked from secrets.tar.enc
-    pgpPublicRing := baseDirectory.value / "pubring.asc", // unpacked from secrets.tar.enc
-    commands += Command.command("commitRelease") { state =>
-      "set com.softwaremill.PublishTravis.isCommitRelease := true" ::
-        "release" ::
-        state
-    },
-    commands += Command.command("publishRelease") { state =>
-      "set com.softwaremill.PublishTravis.isCommitRelease := false" ::
-        "release" ::
-        state
-    },
-    // the steps from Publish.Release.steps are broken down into two segments
-    releaseProcess := {
-      if (isCommitRelease.value) {
-        Seq(
-          checkSnapshotDependencies,
-          inquireVersions,
-          runClean,
-          runTest,
-          setReleaseVersion,
-          updateVersionInDocs(organization.value),
-          commitReleaseVersion,
-          tagRelease,
-          setNextVersion,
-          commitNextVersion,
-          pushChanges
-        )
-      } else {
-        Seq(
-          publishArtifacts,
-          releaseStepCommand("sonatypeBundleRelease")
-        )
-      }
-    }
+  lazy val publishTravisSettings = defaultTravisPublishSettings(
+    Seq(updateVersionInDocs(organization.value))
   )
+
+  def defaultTravisPublishSettings(beforeCommit: Seq[ReleaseStep]) =
+    Seq(
+      isCommitRelease := true,
+      useGpg := false, // use the gpg implementation from the sbt-pgp plugin
+      pgpSecretRing := baseDirectory.value / "secring.asc", // unpacked from secrets.tar.enc
+      pgpPublicRing := baseDirectory.value / "pubring.asc", // unpacked from secrets.tar.enc
+      commands += Command.command("commitRelease") { state =>
+        "set com.softwaremill.PublishTravis.isCommitRelease := true" ::
+          "release" ::
+          state
+      },
+      commands += Command.command("publishRelease") { state =>
+        "set com.softwaremill.PublishTravis.isCommitRelease := false" ::
+          "release" ::
+          state
+      },
+      // the steps from Publish.Release.steps are broken down into two segments
+      releaseProcess := defaultTravisReleaseProcess(beforeCommit)
+    )
+
+  def defaultTravisReleaseProcess(
+      beforeCommit: Seq[ReleaseStep]
+  ): Seq[ReleaseStep] = {
+    if (isCommitRelease.value) {
+      val check = Seq[ReleaseStep](
+        checkSnapshotDependencies,
+        inquireVersions,
+        runClean,
+        runTest,
+        setReleaseVersion
+      )
+      val commitAndPush = Seq[ReleaseStep](
+        commitReleaseVersion,
+        tagRelease,
+        setNextVersion,
+        commitNextVersion,
+        pushChanges
+      )
+      check ++ beforeCommit ++ commitAndPush
+    } else {
+      Seq[ReleaseStep](
+        publishArtifacts,
+        releaseStepCommand("sonatypeBundleRelease")
+      )
+    }
+  }
 }
 
 object PublishTravis extends PublishTravis
