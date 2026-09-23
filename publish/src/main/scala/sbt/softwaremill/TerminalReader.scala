b61cfe5a9a34fdc4ff5e1583072e8a61eeb144c1
pierscin <30854591+pierscin@users.noreply.github.com>
2026-09-23 09:40:08 +0200

    fmt

--- publish/src/main/scala/sbt/softwaremill/TerminalReader.scala
+++ publish/src/main/scala/sbt/softwaremill/TerminalReader.scala
@@ -3,6 +3,7 @@ package sbt.softwaremill
 import sbt.internal.util.{SimpleReader, Terminal}
 
 object TerminalReader {
+
   /** Read input from the terminal of the client that issued the currently running command. */
   def readLine(prompt: String): Option[String] = SimpleReader(Terminal.get).readLine(prompt)
 }
